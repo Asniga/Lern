efb8437cac4c1f125c88c9f1d339bda582350a9d
Asniga <46721075+Asniga@users.noreply.github.com>
2020-05-15 21:58:04 +0300

    changed

--- src/test/java/OpenBrowser.java
+++ src/test/java/OpenBrowser.java
@@ -7,32 +7,63 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.concurrent.TimeUnit;
 
 public class OpenBrowser {
-    WebDriver wd = new ChromeDriver();
+    WebDriver wd;
 
     @Before
-    public void setUp(){
-        wd.get("https://www.maccabi4u.co.il/14-he/Maccabi.aspx");
-        wd.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+    public void setUp() {
+        start();
     }
+
 
     @Test
-    public void chromeBrowserTest(){
-    wd.findElement(By.cssSelector("#ctl00_ucNavigationBar_ucNavigationBarAreaLeft_ancOnline")).click();
-        for (String handle : wd.getWindowHandles()) { wd.switchTo().window(handle);}   // selenium perenosit fokus to next window (tab) in browser
-        wd.findElement(By.cssSelector("#identifyWithPasswordCitizenId")).click();
-        wd.findElement(By.cssSelector("#identifyWithPasswordCitizenId")).clear();
-        wd.findElement(By.cssSelector("#identifyWithPasswordCitizenId")).sendKeys("332300003");
-        wd.findElement(By.cssSelector("#password")).click();
-        wd.findElement(By.cssSelector("#password")).clear();
-        wd.findElement(By.cssSelector("#password")).sendKeys("sinsin");
-
-        wd.findElement(By.xpath("//button[@class='submit validatePassword']")).click();
+    public void chromeBrowserTest() {
+        openSite("https://www.maccabi4u.co.il/14-he/Maccabi.aspx");
+        click(By.cssSelector("#ctl00_ucNavigationBar_ucNavigationBarAreaLeft_ancOnline"));
+        switchWindow();
+        type(By.cssSelector("#identifyWithPasswordCitizenId"),"332300003");
+        type(By.cssSelector("#password"),"sinsin");
+        click(By.xpath("//button[@class='submit validatePassword']"));
     }
+
+
 
 
     @After
     public void tearDown() throws InterruptedException {
         Thread.sleep(6000);
+        stop();
+    }
+
+
+    //-----------------------------------
+    public void start() {
+        wd = new ChromeDriver();
+
+        wd.manage().window().maximize();
+        wd.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+    }
+
+    public void stop() {
         wd.quit();
+    }
+
+    public void openSite(String url) {
+        wd.get(url);
+    }
+
+    public void type(By locator, String text) {
+        wd.findElement(locator).click();
+        wd.findElement(locator).clear();
+        wd.findElement(locator).sendKeys(text);
+    }
+
+    public void switchWindow() {
+        for (String handle : wd.getWindowHandles()) {
+            wd.switchTo().window(handle);
+        }   // selenium perenosit fokus to next window (tab) in browser
+    }
+
+    public void click(By locator) {
+        wd.findElement(locator).click();
     }
 }
